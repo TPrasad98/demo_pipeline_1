6cd1d22841b9dcddd04e2bb4681341cae3157667
TPrasad98 <dastariniprasad7@gmail.com>
2023-07-01 09:34:49 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(name = "cencus_income",
+      version="0.0.1",
+      author_email="dastariniprasad7@gmail.com",
+      packages=find_packages(),
+      install_requires = ["pandas","numpy","flask"]
+      )
